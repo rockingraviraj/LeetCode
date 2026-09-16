e58d39788bea47181d42926a455aa5851ae06602
Ravi Kumar <missionexamc@gmail.com>
2026-09-16 20:08:14 +0530

    Time: 1 ms (99.81%), Space: 50.2 MB (56.77%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,30 +1,28 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        int dp[][] = new int[grid.length][grid[0].length];
-        for(int i=0;i<grid.length;i++){
+        int m = grid.length;
+        int n = grid[0].length;
+        int dp[][] = new int[m][n];
+        for(int i = 0;i<m;i++){
             Arrays.fill(dp[i],-1);
         }
 
-        return Solve(grid,dp,0,0);
+        return Solve(grid,dp,m-1,n-1);
         
     }
-    public int Solve(int[][] grid,int[][] dp ,int row,int col  ){
-        if(row>=grid.length || col>=grid[0].length){
+    private int Solve(int[][]grid,int[][]dp,int row,int col){
+        if(row ==0 && col==0){
+            return grid[0][0];
+        }
+        if(row <0 || col<0){
             return Integer.MAX_VALUE;
         }
-        if(row == grid.length-1 && col == grid[0].length-1){
-            return grid[row][col];
-        }
-
-        if(dp[row][col]!=-1){
+        if(dp[row][col] != -1){
             return dp[row][col];
         }
+        int up = Solve(grid,dp,row-1,col) ;
+        int left =  Solve(grid,dp,row,col-1);
 
-        int right = Solve(grid,dp,row,col+1);
-        int down =  Solve(grid,dp,row+1,col);
-
-        dp[row][col] = grid[row][col] + Math.min(down,right);
-
-        return dp[row][col];
+        return dp[row][col] = Math.min(up,left) + grid[row][col];
     }
 }
